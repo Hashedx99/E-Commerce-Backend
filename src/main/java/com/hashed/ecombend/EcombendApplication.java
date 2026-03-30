48b0afeb644a87961ff7ab751d835fa4a01b1cf4
Hamza Ali <hshabx99@gmail.com>
2026-03-30 16:15:53 +0300

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/com/hashed/ecombend/EcombendApplication.java
@@ -0,0 +1,13 @@
+package com.hashed.ecombend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcombendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcombendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hashed/ecombend/EcombendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashed.ecombend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcombendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
