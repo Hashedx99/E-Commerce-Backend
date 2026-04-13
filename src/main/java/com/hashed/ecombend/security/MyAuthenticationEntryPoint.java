8706f208cbd0a4f691da43cbce8f8e6a71b1a6c3
Hamza Ali <hshabx99@gmail.com>
2026-04-13 12:46:43 +0300

    fix: update ObjectMapper import to use tools.jackson.databind

--- src/main/java/com/hashed/ecombend/security/MyAuthenticationEntryPoint.java
+++ src/main/java/com/hashed/ecombend/security/MyAuthenticationEntryPoint.java
@@ -1,6 +1,6 @@
 package com.hashed.ecombend.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import com.hashed.ecombend.common.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
